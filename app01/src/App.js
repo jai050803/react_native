085808fc447d512a101f1754901c67ad55639079
jai <sjai5803@gmail.com>
2024-05-02 21:42:36 +0530

    react native

---
+++ app01/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Heading from './components/Heading'; // Import the Heading component
+
+function App() {
+  return (
+    <div className="App">
+      <Heading />
+    </div>
+  );
+}
+
+export default App;
