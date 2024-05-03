ac0f1418ccfc7ee6133db475befbf6f98ac596a5
jai <sjai5803@gmail.com>
2024-05-03 12:50:08 +0530

    react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 
 function App() {
-
+  const name = "Intelligence Tool"
   return (
     <>
-      <h1>Hello world</h1>
+    <h1>{name}</h1>
+      <h1>Unordered list</h1>
+      <ul>
+        <li>samosa</li>
+        <li>jalebi</li>
+        <li>fafda</li>
+        <li>gulab jamun</li>
+      </ul>
     </>
   )
 }
