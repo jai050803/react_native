d2969f31e199aebf6e2122c36ce49d49c33f2b33
jai <sjai5803@gmail.com>
2024-05-04 15:46:04 +0530

    react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -15,6 +15,11 @@ function App() {
         <li>fafda</li>
         <li>gulab jamun</li>
       </ul>
+      <img src="../src/assets/download.jpeg" alt="indian food" /><br/>
+      <img src="../src/assets/download (1).jpeg" alt="indian food" /><br/>
+      <img src="../src/assets/download (2).jpeg" alt="indian food" /><br/>
+      <img src="../src/assets/download (3).jpeg" alt="indian food" /><br/>
+      <img src="../src/assets/download (4).jpeg" alt="indian food" />
     </>
   )
 }
---
+++ my-react-app/src/components/Images.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+    return(
+        <>
+            <h1>Indian traditional food thali</h1>
+            <ul>
+                <li>South Indian Thali</li>
+                <img className="Indian-Food" src="../src/assets/download.jpeg" alt="indian food" />
+                <li>Punjabi Thali</li>
+                <img className="Indian-Food" src="../src/assets/download (1).jpeg" alt="indian food" />
+                <li>Bengali Thali</li>
+                <img className="Indian-Food" src="../src/assets/download (2).jpeg" alt="indian food" />
+                <li>Rajasthani Thali</li>
+                <img className="Indian-Food" src="../src/assets/download (3).jpeg" alt="indian food" />
+                <li>Gujarat Food Thali</li>
+                <img className="Indian-Food" src="../src/assets/download (4).jpeg" alt="indian food" />
+            </ul>
+        </>
+    )
+}
+
+export default App
