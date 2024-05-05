0f0ee221a6ad9131d4649b54db5036d9e63689a6
jai <sjai5803@gmail.com>
2024-05-05 14:51:52 +0530

    react attributes

---
+++ my-react-app/src/components/Attributes.jsx
@@ -0,0 +1,10 @@
+import react from 'react'
+
+function App(){
+    return(
+        <>
+            <h1 className='Heading' contentEditable='True'>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dolorum ex cupiditate consectetur earum deserunt debitis commodi non id atque possimus excepturi itaque neque totam nesciunt, sit, minus inventore provident!</p>
+        </>
+    )
+}
