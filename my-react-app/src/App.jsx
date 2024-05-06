fb6ddb9bda99be5cd690fe1ede3a02aae1583fe8
jai <sjai5803@gmail.com>
2024-05-06 19:39:12 +0530

    react

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 
-const text = "";
+let text = "";
 const now = new Date();
 const currentTime = now.getHours();
+
+const customStyle = {
+  color: "",
+}
+
+if (currentTime<=12){
+  text = "Morning"
+  customStyle.color = "red"
+} else if (currentTime>12 & currentTime<18){
+    text = "Afternoon"
+    customStyle.color = "blue"
+} else {
+  text = "evening"
+  customStyle = "Orange"
+}
 
 function App() {
     return(
         <>
-            <h1>Good {currentTime}</h1>
+            <h1>Good {text}</h1>
         </>
     )
 }
